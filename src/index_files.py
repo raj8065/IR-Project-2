22614b8982e84ae984aeff113de16cacd2739494
hailthedawn <kkt7778@rit.edu>
2020-11-04 17:01:08 -0500

    remove punctuation from index terms, include n_postings in index tsv

--- src/index_files.py
+++ src/index_files.py
@@ -1,6 +1,7 @@
 import glob
 import csv
 import os
+import re
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import word_tokenize 
 
@@ -11,6 +12,9 @@ class posting():
 
     def get_posting_list(self):
     	return self.posting_list
+
+    def get_n_postings(self):
+    	return len(self.posting_list)
 
     def record_instance_of_word(self, doc_id):
     	if not self.posting_list.get(doc_id):
@@ -32,9 +36,7 @@ def get_doc_lookup_table(rel_path):
 	  		if row==[]:
 	  			#weed out empty rows
 	  			continue
-
-	  		doc_lookup_table[int(row[0])] = {"heading_id":row[1], "file_name": row[2]}
-	
+	  		doc_lookup_table[int(row[0])] = {"heading_id":row[1], "file_name": row[2]}	
 	
 	return doc_lookup_table
 
@@ -45,8 +47,10 @@ def write_index_to_tsv(inverted_index):
 		tsv_writer = csv.writer(out_file, delimiter='\t')
 		#tsv_writer.writerow(['Keyword', 'Number of Postings', 'Document Ids', 'Frequency'])
 		for key,value in sorted(inverted_index.items()):
-			posting = [key, inverted_index.get(key).get_posting_list()]
-			for key, value in inverted_index[key].get_posting_list().items():
+			posting = [key]
+			p_list = inverted_index[key].get_posting_list()			
+			posting.append(inverted_index[key].get_n_postings())
+			for key, value in p_list.items():
 				# flatten posting dict into list
 				posting.append(key)
 				posting.append(value)
@@ -62,18 +66,16 @@ def get_word_stem(word):
 
 def preprocess_word(word):
 	word = word.lower()
-	word_without_punctuation = re.sub(r'[^\w\s]', '', word)
-	return word_without_punctuation
+	word_alpha_num = re.sub('[^A-Za-z0-9]+', '', word)
+	return word_alpha_num
 
 def gen_index(folder_path):
 
 	inverted_index = {}
-	doc_lookup_table = get_doc_lookup_table("res/doc_lookup.tsv") #TODO change to class member
+	doc_lookup_table = get_doc_lookup_table("res/doc_lookup.tsv")
 
 	transcript_folder = os.path.dirname(os.getcwd())+"\\"+folder_path + "\\"
-
 	ctr = 0
-
 	for doc_id in doc_lookup_table.keys():
 		file_name = transcript_folder + doc_lookup_table.get(doc_id)["file_name"]
 		with open(file_name, 'r') as episode_transcript:
@@ -81,6 +83,8 @@ def gen_index(folder_path):
 			for word in source_code.split():
 				# TODO remove punctuation in word
 				word = preprocess_word(word)
+				if word == '':
+					continue # word contains no alphanumeric characters, and needn't be indexed
 				stem = get_word_stem(word)
 				
 				if not inverted_index.get(stem):
@@ -91,8 +95,8 @@ def gen_index(folder_path):
 				
 			
 		ctr = ctr + 1
-		if ctr == 1: # FOR TESTING PURPOSES 
-			break
+		# if ctr == 100: # FOR TESTING PURPOSES 
+		# 	break
 	
 	# for (key, value) in inverted_index.items(): # FOR TESTING PURPOSES 
 	# 	print(key, value)
@@ -104,6 +108,5 @@ def gen_index(folder_path):
 			
 		
 
-inverted_index = gen_index("out")
-#print(get_doc_lookup_table("res/doc_lookup.tsv"))
+inverted_index = gen_index("res/out")
 write_index_to_tsv(inverted_index)
