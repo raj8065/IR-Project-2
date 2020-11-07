ce2ca423eebd7e563292c88d1a7fe7b27ae20a83
hailthedawn <kkt7778@rit.edu>
2020-11-06 22:15:46 -0500

    run search engine updated to use ultility files.

--- src/run_search_engine.py
+++ src/run_search_engine.py
@@ -5,28 +5,30 @@ import ast
 from src import index_files
 from src.retreival_models import BM25Model
 from src.retreival_models import TfIdfModel
-
+from tsv_reader import Tsv_Reader
 from src.retreival_models import *
 
+tsv_reader = Tsv_Reader("res/doc_lookup.tsv", "res/inverted_index.tsv")
 
 # Gets the inverted index created by index_files.gen_index
 def get_inverted_index(rel_path):
-    inverted_index = {}
-    doc_lookup_file = os.path.join(os.path.dirname(os.getcwd()) + "\\" + rel_path)
-    with open(doc_lookup_file, 'r') as f:
-        idx_data = csv.reader(f, delimiter="\t")
-        for row in idx_data:
-            if not row:
-                # weed out empty rows
-                continue
+    return tsv_reader.get_inv_idx()
+    # inverted_index = {}
+    # doc_lookup_file = os.path.join(os.path.dirname(os.getcwd()) + "\\" + rel_path)
+    # with open(doc_lookup_file, 'r') as f:
+    #     idx_data = csv.reader(f, delimiter="\t")
+    #     for row in idx_data:
+    #         if not row:
+    #             # weed out empty rows
+    #             continue
 
-            inverted_index[row[0]] = [ast.literal_eval(row[1]), row[2], row[3] ]
+    #         inverted_index[row[0]] = [ast.literal_eval(row[1]), row[2], row[3] ]
 
-    return inverted_index
+    # return inverted_index
 
 # Gets the document lookup created by html_parser.write_doc_table_to_tsv
 def get_doc_lookup(rel_path):
-    return index_files.get_doc_lookup_table(rel_path)
+    return tsv_reader.get_doc_lookup_table()
 
 # TODO Generate a html file for search results
 def generate_search_results(rankings, filename):
